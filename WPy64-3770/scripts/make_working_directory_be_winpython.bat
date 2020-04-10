87a90bace485bf73340b3c7d4467ce78c7dca341
Jed Lim <jedlimlx@outlook.com>
2020-04-10 10:31:53 +0800

    Upload WinPython Installation

---
+++ WPy64-3770/scripts/make_working_directory_be_winpython.bat
@@ -0,0 +1,11 @@
+@echo off
+set winpython_ini=%~dp0..\\settings\winpython.ini
+(
+    echo [debug]
+    echo state = disabled
+    echo [environment]
+    echo ## <?> Uncomment lines to override environment variables
+    echo #HOME = %%HOMEDRIVE%%%%HOMEPATH%%\Documents\WinPython%%WINPYVER%%\settings
+    echo #JUPYTER_DATA_DIR = %%HOME%%
+    echo #WINPYWORKDIR = %%HOMEDRIVE%%%%HOMEPATH%%\Documents\WinPython%%WINPYVER%%\Notebooks
+) > "%winpython_ini%"
